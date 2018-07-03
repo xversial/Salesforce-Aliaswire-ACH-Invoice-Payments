dce7a80eb8aea5cae17189dcb6a8f280d3e25b2e
Brandon Xversial <xversial@vionox.com>
2018-07-03 15:34:37 -0700

    Modularize javascript
    Signed-off-by: Brandon Xversial <xversial@vionox.com>

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,18 +1,39 @@
-function submitACHPayment(event) {
-    event.preventDefault();
-    var form = $(this);
+function getToken()
+{
 
+}
+
+function sendData(data)
+{
     $.ajax({
         type: "POST",
         xhrFields: {
             withCredentials: true
         },
         url: form.attr('action'),
-        data: {email: email.val()},
-        dataType: "json"
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: "application/json"
     }).done(function (response) {
         console.log(response);
     });
+}
+
+function submitACHPayment(event) {
+    event.preventDefault();
+    var form = $(this);
+    var accountType = form.find('#AccountType');
+
+    var data = {
+        payment: {
+            method: 'ACH',
+            account: {
+                type: accountType.val(),
+            }
+        },
+        // email: email.val()
+    };
+    sendData(data);
 }
 
 $(document).ready(function () {
