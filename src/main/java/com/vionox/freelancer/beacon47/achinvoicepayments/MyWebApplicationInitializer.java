09f5b34d4b0868887f69c2733a4df4bcde5d61b7
Brandon Xversial <xversial@vionox.com>
2018-07-03 13:08:17 -0700

    Add web application initializer
    Signed-off-by: Brandon Xversial <xversial@vionox.com>

---
+++ src/main/java/com/vionox/freelancer/beacon47/achinvoicepayments/MyWebApplicationInitializer.java
@@ -0,0 +1,41 @@
+package com.vionox.freelancer.beacon47.achinvoicepayments;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+/**
+ * @author Brandon Xversial
+ */
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+    private static final Logger LOG = LoggerFactory.getLogger(MyWebApplicationInitializer.class);
+    @Override
+    public void onStartup(ServletContext container) {
+
+        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+        appContext.setConfigLocation("/WEB-INF/spring/dispatcher-config.xml");
+
+        ServletRegistration.Dynamic registration = container.addServlet("dispatcher", new DispatcherServlet(appContext));
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+
+        /*
+        // Load Spring web application configuration
+        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
+        ac.register(AppConfig.class);
+        ac.refresh();
+
+        // Create and register the DispatcherServlet
+        DispatcherServlet servlet = new DispatcherServlet(ac);
+        ServletRegistration.Dynamic registration = container.addServlet("app", servlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/app/*");
+        */
+    }
+}
