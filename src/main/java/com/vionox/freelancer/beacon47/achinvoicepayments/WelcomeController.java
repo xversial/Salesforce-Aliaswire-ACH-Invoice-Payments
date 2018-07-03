68fcbfc852d2e6cfcbd95fdae5b0d69a53117545
Brandon Xversial <xversial@vionox.com>
2018-07-03 14:09:11 -0700

    add new route for submitting payment
    Signed-off-by: Brandon Xversial <xversial@vionox.com>

--- src/main/java/com/vionox/freelancer/beacon47/achinvoicepayments/WelcomeController.java
+++ src/main/java/com/vionox/freelancer/beacon47/achinvoicepayments/WelcomeController.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
 
@@ -19,9 +21,15 @@ public class WelcomeController {
     @Value("${welcome.message:test}")
     private String message = "Hello World";
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String welcome(Map<String, Object> model) {
         model.put("message", this.message);
         return "welcome";
+    }
+
+    @RequestMapping(value="/api/v1.0/payments/make", method = RequestMethod.POST)
+    public String achMakePayment(@RequestBody String jsonString)
+    {
+        return null;
     }
 }
