dae775e7e86fcc635f97e366af76bd81abb17673
Brandon Xversial <xversial@vionox.com>
2018-07-03 14:11:04 -0700

    Updated annotations on Transaction entity
    Signed-off-by: Brandon Xversial <xversial@vionox.com>

--- src/main/java/com/vionox/freelancer/beacon47/achinvoicepayments/entity/Transaction.java
+++ src/main/java/com/vionox/freelancer/beacon47/achinvoicepayments/entity/Transaction.java
@@ -3,19 +3,25 @@ package com.vionox.freelancer.beacon47.achinvoicepayments.entity;
 import com.google.gson.annotations.SerializedName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 /**
  * @author Brandon Xversial
  */
 
-
+@Entity
 public class Transaction {
     private static final Logger LOG = LoggerFactory.getLogger(Transaction.class);
 
     @SerializedName("billingAccount")
     int billingAccount;
 
-    @SerializedName("billingAccount")
+    @Id
+    @SerializedName("invoiceNumber")
     int invoiceNumber;
     DeviceType deviceType;
 
