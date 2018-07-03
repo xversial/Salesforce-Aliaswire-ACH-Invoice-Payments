d825e70f4b5b4cff02dcb3b5114bf66581030989
Brandon Xversial <xversial@vionox.com>
2018-07-03 13:10:12 -0700

    Add initial base test
    Signed-off-by: Brandon Xversial <xversial@vionox.com>

---
+++ src/test/java/com/vionox/freelancer/beacon47/achinvoicepayments/AchInvoicePaymentsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.vionox.freelancer.beacon47.achinvoicepayments;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AchInvoicePaymentsApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+}
