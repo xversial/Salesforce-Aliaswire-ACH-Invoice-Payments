9c29e82ed5c107784f7498548f0b75434526914b
Brandon Xversial <xversial@vionox.com>
2018-07-07 09:26:52 -0700

    Add address entity
    Signed-off-by: Brandon Xversial <xversial@vionox.com>

---
+++ src/main/java/com/vionox/freelancer/beacon47/achinvoicepayments/entity/Address.java
@@ -0,0 +1,11 @@
+package com.vionox.freelancer.beacon47.achinvoicepayments.entity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Brandon Xversial
+ */
+public class Address {
+    private static final Logger LOG = LoggerFactory.getLogger(Address.class);
+}
