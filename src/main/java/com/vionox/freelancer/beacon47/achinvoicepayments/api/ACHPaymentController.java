10d7974d72ce39af188632aedb6711c0bd021ca3
Brandon Xversial <xversial@vionox.com>
2018-07-06 18:11:12 -0700

    Parse JSON and fetch token
    Signed-off-by: Brandon Xversial <xversial@vionox.com>

--- src/main/java/com/vionox/freelancer/beacon47/achinvoicepayments/api/ACHPaymentController.java
+++ src/main/java/com/vionox/freelancer/beacon47/achinvoicepayments/api/ACHPaymentController.java
@@ -1,5 +1,9 @@
 package com.vionox.freelancer.beacon47.achinvoicepayments.api;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,16 +11,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 /**
  * @author Brandon Xversial
  */
 @RestController
 public class ACHPaymentController {
     private static final Logger LOG = LoggerFactory.getLogger(ACHPaymentController.class);
+    ObjectMapper mapper = new ObjectMapper();
 
     @RequestMapping(value = "/api/v1.0/payments/make", method = {RequestMethod.POST})
     public String achMakePayment(@RequestBody String jsonString)
     {
+        try {
+            ObjectNode node = mapper.readValue(jsonString, ObjectNode.class);
+            JsonNode payment = node.get("payment");
+            String token = payment.get("token").asText();
+            LOG.info(token);
+        } catch (IOException ex) {
+            LOG.warn(ExceptionUtils.getStackTrace(ex));
+        }
         return null;
     }
 }
