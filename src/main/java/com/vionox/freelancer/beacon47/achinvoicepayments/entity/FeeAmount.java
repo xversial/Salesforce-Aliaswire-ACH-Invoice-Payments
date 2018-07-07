dfe7b319ad45b43defac4956df8e9c14ff57d7fb
Brandon Xversial <xversial@vionox.com>
2018-07-07 16:23:09 -0700

    Add fee amount entity
    Signed-off-by: Brandon Xversial <xversial@vionox.com>

---
+++ src/main/java/com/vionox/freelancer/beacon47/achinvoicepayments/entity/FeeAmount.java
@@ -0,0 +1,11 @@
+package com.vionox.freelancer.beacon47.achinvoicepayments.entity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Brandon Xversial
+ */
+public class FeeAmount {
+    private static final Logger LOG = LoggerFactory.getLogger(FeeAmount.class);
+}
