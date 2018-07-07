44a4dc24329fce90103c681bf11c2e95782b0a2e
Brandon Xversial <xversial@vionox.com>
2018-07-07 09:29:19 -0700

    Add Funding Account entity
    Signed-off-by: Brandon Xversial <xversial@vionox.com>

---
+++ src/main/java/com/vionox/freelancer/beacon47/achinvoicepayments/entity/FundingAccount.java
@@ -0,0 +1,71 @@
+package com.vionox.freelancer.beacon47.achinvoicepayments.entity;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+
+/** @author Brandon Xversial */
+public class FundingAccount implements Serializable {
+  private static final Logger LOG = LoggerFactory.getLogger(FundingAccount.class);
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  @SerializedName("type")
+  @JsonProperty("type")
+  private final AccountType type;
+
+  @SerializedName("number")
+  @JsonProperty("number")
+  private final String token;
+
+  @SerializedName("display")
+  @JsonProperty("display")
+  private final String name;
+
+  @SerializedName("address")
+  @JsonProperty("address")
+  private final Address address;
+
+  @JsonCreator
+  public FundingAccount(
+      @JsonProperty("type") AccountType type,
+      @JsonProperty("number") String token,
+      @JsonProperty("display") String name,
+      @JsonProperty("address") Address address) {
+    this.type = type;
+    this.token = token;
+    this.name = name;
+    this.address = address;
+  }
+
+  @JsonCreator
+  public FundingAccount(
+      @JsonProperty("number") String token,
+      @JsonProperty("display") String name,
+      @JsonProperty("address") Address address) {
+    this.type = AccountType.CREDIT_CARD;
+    this.token = token;
+    this.name = name;
+    this.address = address;
+  }
+
+  public String getToken() {
+    return token;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Address getAddress() {
+    return address;
+  }
+
+  public AccountType getType() {
+    return type;
+  }
+}
