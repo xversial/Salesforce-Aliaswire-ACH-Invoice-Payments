f5c4f0a34051372c7835ab409c162391ccdae388
Brandon Xversial <xversial@vionox.com>
2018-07-07 09:43:46 -0700

    Annotate Jackson property names on AccountType values
    Signed-off-by: Brandon Xversial <xversial@vionox.com>

--- src/main/java/com/vionox/freelancer/beacon47/achinvoicepayments/entity/AccountType.java
+++ src/main/java/com/vionox/freelancer/beacon47/achinvoicepayments/entity/AccountType.java
@@ -1,11 +1,13 @@
 package com.vionox.freelancer.beacon47.achinvoicepayments.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * @author Brandon Xversial
  */
 public enum AccountType {
-    ACH(),
-    CREDIT_CARD();
+    @JsonProperty("ACH") ACH(),
+    @JsonProperty("CREDIT_CARD") CREDIT_CARD();
 
     AccountType() {
     }
