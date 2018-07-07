5a25d3ac92e37cad048a89c0856204a2772d6147
Brandon Xversial <xversial@vionox.com>
2018-07-07 09:26:02 -0700

    Add funding account type
    Signed-off-by: Brandon Xversial <xversial@vionox.com>

---
+++ src/main/java/com/vionox/freelancer/beacon47/achinvoicepayments/entity/AccountType.java
@@ -0,0 +1,12 @@
+package com.vionox.freelancer.beacon47.achinvoicepayments.entity;
+
+/**
+ * @author Brandon Xversial
+ */
+public enum AccountType {
+    ACH(),
+    CREDIT_CARD();
+
+    AccountType() {
+    }
+}
