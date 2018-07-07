6580593d351e042b5ae381539fc3be9f0bc1ac98
Brandon Xversial <xversial@vionox.com>
2018-07-06 17:35:51 -0700

    Token is now generated with AliasWire
    Signed-off-by: Brandon Xversial <xversial@vionox.com>

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,3 +1,87 @@
+var PUBLIC_ALIAS_WIRE_KEY = 'pk_test_59a973aa4b8bf9fa80bea9fd9cbd4ec8bf619b7fc6720320e3424178';
+var serverAPIEndpoint = null;
+
+function __awGetToken(form) {
+
+    /////////////////////////////////////////////////////////////////////////
+    //// Collect Input Variables (using JavaScript or jQuery)
+    ////////////////////////////////////////////////////////////////////////
+
+    // -------------------------------------------------------------------
+    // javascript style of getting value from form using id.
+    // alert(document.getElementById('cardNo').value);
+
+    //jquery style of getting value by id
+    //alert($('#cardNo').val());
+    // -------------------------------------------------------------------
+
+    //get the credit card number, expiration, zip code, CVV etc values from html page
+    var accountType = form.find('#AccountType').val();
+    var accountNumber = form.find('#AccountNumber').val();
+    var routingNumber = form.find('#RoutingNumber').val();
+
+    /////////////////////////////////////////////////////////////////////////
+    //// Build the API Object
+    ////////////////////////////////////////////////////////////////////////
+
+    //aliaswire javascript REST based api object
+    var aw = new AWAPI.TS();
+
+    //create credit card account token object
+    var awTokenObject = aw.createAchAccountToken(accountNumber, routingNumber, accountType);
+
+    /////////////////////////////////////////////////////////////////////////
+    //// Send the request to get a token
+    ////////////////////////////////////////////////////////////////////////
+
+    // send the request using published key. The token is returned to the response handler
+    aw.requestToken(
+        awTokenObject,
+        __awResponse,
+        PUBLIC_ALIAS_WIRE_KEY,
+        'aw-publishedkey');
+
+}
+function __awResponse(obj, resultCode, detailReason, str, httpStatus) {
+    if (resultCode == "A000") {
+        debugger;
+        var token = obj.getToken();
+        /*
+        $('#sccmsg').html('<b> SUCCESS : ' + obj.getToken().getNumber() + '</b>');
+        var info = "<br><span>";
+        info = info + str + "</span>";
+        $('#sccmsg').append(info);
+        $('#sccmsg').css('display', 'block');
+        $('#sccmsg').delay(10000).fadeOut();
+        */
+
+
+        var data = {
+            payment: {
+                method: 'ACH',
+                account: {
+                    // type: accountType.val(),
+                },
+                token: token.getNumber(),
+            },
+            // email: email.val()
+        };
+        sendData(data);
+
+        //////////////////////////////////////////////////
+        // alert("Token = " + obj.getToken().getNumber());
+        //////////////////////////////////////////////////
+    } else
+
+    {
+        $('#errmsg').html('<b>' + "Error: ResultCode[" + resultCode + "] DetailReason[" + detailReason + "]" + '</b>');
+        $('#errmsg').css('display', 'block');
+        $('#errmsg').delay(10000).fadeOut();
+        //alert("Error: ResultCode[" + resultCode + "] DetailReason[" + detailReason + "]");
+    }
+}
+
+
 function getToken()
 {
 
@@ -10,7 +94,7 @@ function sendData(data)
         xhrFields: {
             withCredentials: true
         },
-        url: form.attr('action'),
+        url: serverAPIEndpoint,
         data: JSON.stringify(data),
         dataType: "json",
         contentType: "application/json"
@@ -22,18 +106,9 @@ function sendData(data)
 function submitACHPayment(event) {
     event.preventDefault();
     var form = $(this);
-    var accountType = form.find('#AccountType');
 
-    var data = {
-        payment: {
-            method: 'ACH',
-            account: {
-                type: accountType.val(),
-            }
-        },
-        // email: email.val()
-    };
-    sendData(data);
+    serverAPIEndpoint = form.attr('action');
+    var token = __awGetToken(form);
 }
 
 $(document).ready(function () {
